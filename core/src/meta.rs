c5210048520193d50bc10e75c547ae472632c06a
James MacMahon <james@oxide.computer>
2025-07-21 21:33:27 +0000

    add Meta

---
+++ core/src/meta.rs
@@ -0,0 +1,17 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use super::*;
+
+#[derive(Serialize, JsonSchema, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Meta {
+    pub resource_type: String,
+
+    // TODO:
+    // - created
+    // - lastModified
+    // - location (optional)
+    // - version (optional)
+}
