130230c350713d51b7d9153490edfd9530a775b0
James MacMahon <james@oxide.computer>
2025-07-23 14:05:07 +0000

    accept StatusCode instead of u16

--- core/src/response.rs
+++ core/src/response.rs
@@ -194,8 +194,8 @@ pub struct Error {
 }
 
 impl Error {
-    pub fn new(
-        status: u16,
+    fn new(
+        status: StatusCode,
         error_type: Option<ErrorType>,
         detail: String,
     ) -> Self {
@@ -203,30 +203,38 @@ impl Error {
             schemas: vec![String::from(
                 "urn:ietf:params:scim:api:messages:2.0:Error",
             )],
-            status: status.to_string(),
+            status: status.as_str().to_string(),
             error_type,
             detail,
         }
     }
 
     pub fn invalid_filter(detail: String) -> Self {
-        Self::new(400, Some(ErrorType::InvalidFilter), detail)
+        Self::new(
+            StatusCode::BAD_REQUEST,
+            Some(ErrorType::InvalidFilter),
+            detail,
+        )
     }
 
     pub fn not_found(id: String) -> Self {
-        Self::new(404, None, format!("Resource {id} not found"))
+        Self::new(
+            StatusCode::NOT_FOUND,
+            None,
+            format!("Resource {id} not found"),
+        )
     }
 
     pub fn conflict(identifier: String) -> Self {
         Self::new(
-            409,
+            StatusCode::CONFLICT,
             Some(ErrorType::Uniqueness),
             format!("Resource matching {identifier} exists already"),
         )
     }
 
     pub fn internal_error(detail: String) -> Self {
-        Self::new(500, None, detail)
+        Self::new(StatusCode::INTERNAL_SERVER_ERROR, None, detail)
     }
 
     pub fn status(&self) -> Result<u16, std::num::ParseIntError> {
